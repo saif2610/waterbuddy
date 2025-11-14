5f411bc52246270da4ec06435b9e1bb55cb8c7b4
saif2610 <mohammed@jainvidyalaya.in>
2025-11-14 12:37:42 +0530

    Update waterbuddy-ai.py

--- waterbuddy/waterbuddy-ai.py
+++ waterbuddy/waterbuddy-ai.py
@@ -18,11 +18,10 @@ LOGS_FILE = "logs.json"
 BADGES_FILE = "badges.json"
 
 # ---------------- IMAGE PATHS ----------------
-MASCOT_SAD = "Water_Dragon_Sad_Slim.jpg"
-MASCOT_LITTLE_HAPPY = "Water_Dragon_Little_Happy_Slim.jpg"
-MASCOT_HAPPY = "Water_Dragon_Happy.jpg"
-MASCOT_STARE = "Water_Dragon_Angry_Cute_Stare.jpg"
-
+MASCOT_SAD = "C:\Users\Ammi\Downloads\mascot\Water_Dragon_Sad_Slim.jpg"
+MASCOT_LITTLE_HAPPY = "C:\Users\Ammi\Downloads\mascot\Water_Dragon_Little_Happy_Slim.jpg"
+MASCOT_HAPPY = "C:\Users\Ammi\Downloads\mascot\Water_Dragon_Happy.jpg"
+MASCOT_STARE = "C:\Users\Ammi\Downloads\mascot\Water_Dragon_Angry_Cute_Stare.jpg"
 st.set_page_config(page_title="💧 Water Buddy", page_icon="💦", layout="centered")
 
 # ---------------- BEAUTIFUL CSS ----------------
@@ -294,3 +293,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
